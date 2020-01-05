e5a857d7f26aa576fa8c3e2a630bb580456cd44c
Michaelmical <michael.cortesiano21@gmail.com>
2020-01-05 21:24:27 +0800

    push

---
+++ resources/views/functionpoints-create.blade.php
@@ -0,0 +1,116 @@
+@extends('layouts.admin');
+
+@push('meta')
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+@endpush
+@push('ui')
+    <link rel="stylesheet" href="{{ asset('plugins/select2/css/select2.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('plugins/daterangepicker/daterangepicker.css') }}">
+@endpush
+
+@section('content')
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <!-- left column -->
+                <div class="col-md-12">
+                    <!-- general form elements -->
+                    <div class="card card-primary">
+                        <div class="card-header">
+                            <h3 class="card-title">Build Details</h3>
+                        </div>
+                        <!-- /.card-header -->
+
+                        @if (count($errors) > 0)
+                            <div class="alert alert-danger">
+                                <strong>Whoops!</strong> There were some problems with your input.
+                                <ul>
+                                    @foreach ($errors->all() as $error)
+                                        <li>{{ $error }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        @endif
+                        <!-- form start -->
+                        <form id="buildadd" method="POST" action="{{route('build.store')}}">
+                            @csrf
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-sm-3">
+                                        <div class="form-group">
+                                            <label>Projects</label>
+                                            <select class="form-control select2bs4" style="width: 100%;" name="inputProject">
+                                                <option selected="selected" disabled="disabled" value="null"></option>
+                                                @foreach($projects as $project)
+                                                    <option value="{{ $project->proj_id }}">{{ $project->proj_name }}</option>
+                                                @endforeach
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-3">
+                                        <div class="form-group">
+                                            <label for="inputSP">Service Pack</label>
+                                            <input type="text" class="form-control" name="inputSP" placeholder="Enter SP..">
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-3">
+                                        <div class="form-group">
+                                            <label for="inputVS">Version</label>
+                                            <input type="text" class="form-control" name="inputVS" placeholder="Enter Version..">
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-3">
+                                        <div class="form-group">
+                                            <label for="inputDrop">Drop</label>
+                                            <input type="text" class="form-control" name="inputDrop" placeholder="Enter Drop..">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-sm-3">
+                                        <div class="form-group">
+                                            <label for="inputDescr">Description</label>
+                                            <input type="text" class="form-control" name="inputDescr" placeholder="Enter Description..">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- /.card-body -->
+                            <div class="card-footer">
+                                <button type="submit" class="btn btn-primary" id="btn-submit">Add Build</button>
+                            </div>
+                        </form>
+                    </div>
+                    <!-- /.card -->
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
+
+@push('addons')
+    <script src="{{asset('plugins/bs-custom-file-input/bs-custom-file-input.min.js')}}"></script>
+    <script type="text/javascript">
+        $(document).ready(function () {
+            bsCustomFileInput.init();
+        });
+    </script>
+    <script src="{{asset('plugins/select2/js/select2.full.min.js')}}"></script>
+    <script src="{{asset('plugins/daterangepicker/daterangepicker.js')}}"></script>
+    <script src="{{asset('plugins/moment/moment.min.js')}}"></script>
+    <script src="{{asset('plugins/inputmask/min/jquery.inputmask.bundle.min.js')}}"></script>
+    <script>
+        $('.select2').select2();
+        $('.select2bs4').select2({
+            theme: 'bootstrap4'
+        });
+        //Datemask yyyy/mm/dd
+        $('#datemask').inputmask('yyyy/mm/dd', { 'placeholder': 'yyyy/mm/dd' });
+        //Datemask2 yyyy/mm/dd
+        $('#datemask2').inputmask('yyyy/mm/dd', { 'placeholder': 'yyyy/mm/dd' });
+        //Money Euro
+        $('[data-mask]').inputmask()
+    </script>
+@endpush
+
---
+++ resources/views/functionpoints.blade.php
@@ -0,0 +1,135 @@
+@extends('layouts.admin')
+
+@push('ui')
+    <link rel="stylesheet" href="{{ asset('plugins/datatables-bs4/css/dataTables.bootstrap4.css') }}">
+@endpush
+
+@push('meta')
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+@endpush
+
+@section('content')
+    <section class="content">
+        <div class="modal fade" id="modal-danger">
+            <div class="modal-dialog">
+                <div class="modal-content bg-danger">
+                    <div class="modal-header">
+                        <h4 class="modal-title">Confirmation</h4>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <p>Are you sure you want to delete this record?</p>
+                    </div>
+                    <div class="modal-footer justify-content-between">
+                        <button type="button" class="btn btn-outline-light" data-dismiss="modal" id="btnCloseModal">Cancel</button>
+                        @csrf
+                        @method('DELETE')
+                        <button type="button" class="btn btn-outline-light" id="deleteRecord">Delete</button>
+                    </div>
+                </div>
+                <!-- /.modal-content -->
+            </div>
+            <!-- /.modal-dialog -->
+        </div>
+
+        <div class="row">
+            <div class="col-12">
+                <div class="card card-primary card-outline">
+                    <div class="card-header">
+                        <h3 class="card-title">BUILDS</h3>
+                    </div>
+                    <div class="card-body">
+                        <table id="example1" class="table table-bordered table-striped">
+                            <thead>
+                            <tr>
+                                <th>Project Name</th>
+                                <th>Build Description</th>
+                                <th>Actions</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach($builds as $build)
+                                <tr>
+                                    <td>{{$build->proj_name}}</td>
+                                    <td>{{$build->descr}}</td>
+                                    <td>
+                                        <a class="btn btn-warning" href="{{route('build.edit',['id'=>$build->build_id])}}">
+                                            <i class="fas fa-edit"></i>
+                                        </a>
+                                        <a class="btn btn-danger deleteBuild" data-toggle="modal" data-target="#modal-danger" id="{{$build->build_id}}">
+                                            <i class="fas fa-trash"></i>
+                                        </a>
+                                    </td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                            <tfoot>
+                            <tr>
+                                <th>Project Name</th>
+                                <th>Build Description</th>
+                                <th>Actions</th>
+                            </tr>
+                            </tfoot>
+                        </table>
+                    </div>
+                    <div class="card-footer">
+                        <a class="btn btn-success" href="{{route('build-create')}}">
+                            <i class="fas fa-plus-square"></i> Add Build
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
+
+@push('addons')
+    <script src="{{asset('plugins/datatables/jquery.dataTables.js')}}"></script>
+    <script src="{{asset('plugins/datatables-bs4/js/dataTables.bootstrap4.js')}}"></script>
+    <script>
+        $(function () {
+            $("#example1").DataTable();
+            $('#example2').DataTable({
+                "paging": true,
+                "lengthChange": false,
+                "searching": false,
+                "ordering": true,
+                "info": true,
+                "autoWidth": false,
+            });
+        });
+
+        $('body').on('click', '.deleteBuild', function () {
+            $('#deleteRecord').attr('data-id', $(this).attr("id"));
+        });
+
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+
+        $('#deleteRecord').click(function () {
+            $.ajax({
+                type: "DELETE",
+                url: '/build/' + $('#deleteRecord').attr('data-id'),
+                beforeSend:function(){
+                    $('#deleteRecord').text('Deleting...');
+                },
+                success: function (data) {
+                    if(data.success){
+                        $('#btnCloseModal').click();
+                        location.reload();
+                    }
+                },
+                error: function (data) {
+                    // console.log('Error:', data);
+                }
+            });
+        });
+
+    </script>
+@endpush
+
